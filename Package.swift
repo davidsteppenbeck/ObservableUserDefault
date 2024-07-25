8da52af4b9c0c575eec9da06059615982959b28f
Mihai Șeremet <mihai8804858@gmail.com>
2024-07-25 18:18:25 +0100

    Update origin repo for swift-syntax

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
     ],
     dependencies: [
         // Depend on the latest Swift 5.10 release of SwiftSyntax
-        .package(url: "https://github.com/apple/swift-syntax.git", from: "510.0.0"),
+        .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "510.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
